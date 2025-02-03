63d209f38c84fcefb66578bc7de21e66f2eb724d
Flavio Costa <flaviojcostafilho@gmail.com>
2025-02-03 14:27:22 -0300

    feat: Add infrastructure configs

---
+++ BloodDonationSystem.Infrastructure/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using BloodDonationSystem.Domain.Repositories;
+using BloodDonationSystem.Infrastructure.Persistance;
+using BloodDonationSystem.Infrastructure.Persistance.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BloodDonationSystem.Infrastructure
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSqlServer(configuration);
+            services.AddRepositories();
+            return services;
+        }
+
+        private static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IAddressRepository, AddressRepository>();
+
+
+            return services;
+        }
+
+        private static IServiceCollection AddSqlServer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BloodDonationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
+
+    }
+}
---
+++ BloodDonationSystem.Infrastructure/Persistance/Repositories/AddressRepository.cs
@@ -0,0 +1,37 @@
+﻿using BloodDonationSystem.Domain.Entities;
+using BloodDonationSystem.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodDonationSystem.Infrastructure.Persistance.Repositories
+{
+    public class AddressRepository : IAddressRepository
+    {
+        private readonly BloodDonationDbContext _bloodDonationDbContext;
+        public AddressRepository(BloodDonationDbContext bloodDonationDbContext)
+        {
+            _bloodDonationDbContext = bloodDonationDbContext;
+        }
+
+        public async Task CreateAsync(Address entity)
+        {
+            await _bloodDonationDbContext.Address.AddAsync(entity);
+
+            await _bloodDonationDbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<Address>> GetAllAsync()
+        {
+            return await _bloodDonationDbContext.Address.Where(x => x.IsActive).ToListAsync();
+        }
+
+        public async Task<Address> GetByIdAsync(Guid id)
+        {
+            return await _bloodDonationDbContext.Address.SingleOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _bloodDonationDbContext.SaveChangesAsync();
+        }
+    }
+}
