adcc83e8f65a4689ed6bffb654b589839c04359c
Flavio Costa <flaviojcostafilho@gmail.com>
2025-02-03 15:37:24 -0300

    config: Add migration and config to database

---
+++ BloodDonationSystem.API/Controllers/AddressController.cs
@@ -0,0 +1,39 @@
+﻿using BloodDonationSystem.Application.Queries.GetAddressById;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloodDonationSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AddressController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            try
+            {
+                var getAddressByIdQuery = new GetAddressByIdQuery(id);
+                var address = await _mediator.Send(getAddressByIdQuery);
+
+                if (address == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(address);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
