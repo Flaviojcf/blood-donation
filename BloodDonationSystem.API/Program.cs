b4dcc029b4ff007c04eef2a37d8d240308e68916
Flavio Costa <flaviojcostafilho@gmail.com>
2025-02-03 17:08:58 -0300

    feat: Add Extensions config for web api

--- BloodDonationSystem.API/Program.cs
+++ BloodDonationSystem.API/Program.cs
@@ -1,29 +1,15 @@
-using BloodDonationSystem.API.Filter;
-using BloodDonationSystem.Application.Commands.CreateAddress;
-using BloodDonationSystem.Application.Validators.Address;
+using BloodDonationSystem.API;
 using BloodDonationSystem.Infrastructure;
-using FluentValidation.AspNetCore;
-using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers(options =>
-{
-    options.Filters.Add<TrimStringsActionFilter>();
-    options.Filters.Add(typeof(ValidationFilter));
-})
-.AddFluentValidation(fv =>
-{
-    fv.RegisterValidatorsFromAssemblyContaining<CreateAddressCommandValidator>();
-});
-
+builder.Services.AddWebApi(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddMediatR(typeof(CreateAddressCommand));
+
 
 var app = builder.Build();
 
